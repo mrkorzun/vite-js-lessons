52024a3fb60570f6899364027a672f371d37d9a6
mrkorzun <60797864+mrkorzun@users.noreply.github.com>
2026-04-23 14:22:44 +0000

    Deploying to gh-pages from @ mrkorzun/vite-js-lessons@ff06ae810f8de34963e1c303e2bed425e45ef665 🚀

---
+++ assets/refs-C_mHC-55.js
@@ -0,0 +1,2 @@
+const e={timeClock:document.querySelector(".current-time"),banner:document.querySelector(".hint-banner")};export{e as r};
+//# sourceMappingURL=refs-C_mHC-55.js.map
---
+++ task1.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{r as i}from"./assets/refs-C_mHC-55.js";const e=t=>String(t).padStart(2,"0"),n=()=>{const t=new Date,r=e(t.getHours()),s=e(t.getMinutes()),c=e(t.getSeconds());return`${r}:${s}:${c}`};n();const o=()=>{i.timeClock.textContent=n()};o();setInterval(o,1e3);
+//# sourceMappingURL=task1.js.map
